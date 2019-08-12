8834d77295c051d39b1d35cf408e8c9e99caeb54
maocq <mauriciocarmona@seven4n.com>
2019-08-12 09:56:22 -0500

    Actualización

--- app/domain/commands/RegisterProjectCommand.scala
+++ app/domain/commands/RegisterProjectCommand.scala
@@ -1,17 +1,30 @@
 package domain.commands
 
-import domain.model.TransformerDomain
+import cats.implicits._
+import domain.model.{GError, TransformerDomain}
 import domain.services.ProjectService
+import implicits.implicits._
 import infrastructure.ProjectIDDTO
 import javax.inject.Inject
 import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 import play.api.libs.json.Json
+
 
 class RegisterProjectCommand @Inject()(projectService: ProjectService) extends Command[ProjectIDDTO] with TransformerDomain {
 
   def execute(dto: ProjectIDDTO): Task[Consequence] = {
-
-    projectService.register(dto.id)
+    (for {
+        x <- projectService.register(dto.id)
+        _ <- updateInfoProject(dto.id).toEitherT
+    } yield x)
       .fold(leftConsequence, r => rightConsequence(Json.toJson(r)))
+  }
+
+  def updateInfoProject(projectID: Int) = {
+    Task.eval{
+      projectService.registerCommits(projectID).value.runToFuture
+      "ok".asRight[GError]
+    }
   }
 }
--- app/domain/model/TransformerDomain.scala
+++ app/domain/model/TransformerDomain.scala
@@ -5,5 +5,7 @@ import play.api.libs.json.Json
 trait TransformerDomain {
 
   implicit val projectReads = Json.format[Project]
+  implicit val commitReads = Json.format[Commit]
+  implicit val diffReads = Json.format[Diff]
 
 }
